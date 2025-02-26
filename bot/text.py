495f59f4cfe40f733e29ff3963701ff7197ffb81
HerokuBot <134994186+HerokuZone@users.noreply.github.com>
2025-02-26 12:55:27 +0530

    Create text.py

---
+++ bot/text.py
@@ -0,0 +1,13 @@
+TEXT = {
+    "INVALID_TOKEN": "<b>Access Token is not yours!</b>\n\n<i>Kindly generate your own to use.</i>",
+    "USED_TOKEN": "<b>Access Token already used!</b>\n\n<i>Kindly generate a new one.</i>",
+    "TOKEN_SUCCESS": """⌬ Access Login Token : 
+    │
+    ┟ <b>Status</b> → <code>Generated Successfully</code>
+    ┟ <b>Access Token</b> → <code>{}</code>
+    ┃
+    ┖ <b>Validity:</b> {}""",
+    "START_AUTH": "This bot can mirror from links|tgfiles|torrents|nzb|rclone-cloud to any rclone cloud, Google Drive or to telegram.\nType /help to get a list of available commands",
+    "START_PM": "<i>Now, Bot will send you all your files and links here. Start Using Now...</i>",
+    "START_UNAUTHORIZED": "<i>Bot can mirror/leech from links|tgfiles|torrents|nzb|rclone-cloud to any rclone cloud, Google Drive or to telegram.\n\n⚠️ You Are not authorized user! Deploy your own WZML-X bot</i>",
+}
